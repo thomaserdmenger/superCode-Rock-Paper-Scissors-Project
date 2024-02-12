424d5619e9617e312a688938015c2e869a445736
thomaserdmenger <thomas.erdmenger@googlemail.com>
2024-02-12 09:54:06 +0100

    Calculate Random Number

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,21 @@
 const reloadBtn = document.querySelector('input[type="button"]');
+const rockBtn = document.querySelector('#rock');
+const paperBtn = document.querySelector('#paper');
+const scissorsBtn = document.querySelector('#scissors');
 
 // ! Reload Webpage
 reloadBtn.addEventListener('click', () => window.location.reload());
 
+// ! Get Random Nummer between 1-3
+const randomNum = () => {
+  const randomNumber = Math.ceil(Math.random() * 3);
+  return randomNumber;
+};
+
+// ! Get the Winner
+const getWinner = () => {};
+
 // ! Event Listener Functions
+rockBtn.addEventListener('click', getWinner);
+paperBtn.addEventListener('click', getWinner);
+scissorsBtn.addEventListener('click', getWinner);
