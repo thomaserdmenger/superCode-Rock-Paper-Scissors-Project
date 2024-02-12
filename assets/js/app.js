1fa7da3ef56130b941b8cc761b40adb59c20643d
thomaserdmenger <thomas.erdmenger@googlemail.com>
2024-02-12 10:31:00 +0100

    Calculate the Winner and Render to Screen

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,18 +2,60 @@ const reloadBtn = document.querySelector('input[type="button"]');
 const rockBtn = document.querySelector('#rock');
 const paperBtn = document.querySelector('#paper');
 const scissorsBtn = document.querySelector('#scissors');
+const resultsContainer = document.querySelector('.results');
 
 // ! Reload Webpage
 reloadBtn.addEventListener('click', () => window.location.reload());
 
+// ! Render Result
+const renderResult = (userResult, computerResult, finalResult) => {
+  document.querySelector('.results').innerHTML = '';
+
+  const result = `
+    <p>You choose: ${userResult}</p>
+    <p>CPU chosse: ${computerResult}</p>
+    <p>Final Result: ${finalResult}</p>
+  `;
+
+  document.querySelector('.results').innerHTML = result;
+};
+
 // ! Get Random Nummer between 1-3
-const randomNum = () => {
+const randomCPUAction = () => {
   const randomNumber = Math.ceil(Math.random() * 3);
-  return randomNumber;
+
+  if (randomNumber === 1) return 'rock';
+  else if (randomNumber === 2) return 'paper';
+  else return 'scissors';
 };
 
 // ! Get the Winner
-const getWinner = () => {};
+const getWinner = (event) => {
+  const userChoise = event.target.id;
+  const computerChoice = randomCPUAction();
+  const winner = 'You are the winner 🥳';
+  const looser = 'The computer wins 😩';
+  const draw = 'Draw! But you are the winner of hearts 💖';
+  let finalResult;
+
+  if (userChoise === computerChoice) {
+    finalResult = draw;
+  } else if (userChoise === 'rock' && computerChoice === 'paper') {
+    finalResult = looser;
+  } else if (userChoise === 'rock' && computerChoice === 'scissors') {
+    finalResult = winner;
+  } else if (userChoise === 'paper' && computerChoice === 'rock') {
+    finalResult = winner;
+  } else if (userChoise === 'paper' && computerChoice === 'scissors') {
+    finalResult = looser;
+  } else if (userChoise === 'scissors' && computerChoice === 'rock') {
+    finalResult = looser;
+  } else if (userChoise === 'scissors' && computerChoice === 'paper') {
+    finalResult = winner;
+  }
+
+  renderResult(userChoise, computerChoice, finalResult);
+};
 
 // ! Event Listener Functions
 rockBtn.addEventListener('click', getWinner);
