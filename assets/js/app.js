890a45a42eb4e8cc16c938a7c06ba8114c7d591e
thomaserdmenger <thomas.erdmenger@googlemail.com>
2024-02-12 09:46:15 +0100

    Restart Game in Click

---
+++ assets/js/app.js
@@ -0,0 +1,6 @@
+const reloadBtn = document.querySelector('input[type="button"]');
+
+// ! Reload Webpage
+reloadBtn.addEventListener('click', () => window.location.reload());
+
+// ! Event Listener Functions
